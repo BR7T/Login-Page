c9069fe4434b171a10f365cb5c851d5d85a52e42
BR7T <buratinivictor@gmail.com>
2024-02-16 19:27:28 -0300

    start reponsive

--- script.js
+++ script.js
@@ -1,72 +1,56 @@
-const circle = document.getElementById('bar')
-const container = document.getElementById( 'switch')
-const card = document.querySelector('#card-container')
+const circle = document.getElementById("bar");
+const container = document.getElementById("switch");
+const card = document.querySelector("#card-container");
 
 let PorcTime = 0;
-let cresce = false
-let changing = false
+let cresce = false;
+let changing = false;
 let LoopTimer;
-container.addEventListener('mousedown' , OnMouseOver)
-container.addEventListener('mouseup',OnMouseOut)
+container.addEventListener("mousedown", OnMouseOver);
+container.addEventListener("mouseup", OnMouseOut);
 
-
-
-function OnMouseOver(){
-    if(!changing || !cresce){
-        clearTimeout(LoopTimer)
-        cresce = true
-        changing = true
-        changeSize()    
-    }
-   
+function OnMouseOver() {
+  if (!changing || !cresce) {
+    clearTimeout(LoopTimer);
+    cresce = true;
+    changing = true;
+    changeSize();
+  }
 }
-function OnMouseOut(){
-    if(changing || cresce){
-        clearTimeout(LoopTimer)
-        cresce = false
-        changing = true
+function OnMouseOut() {
+  if (changing || cresce) {
+    clearTimeout(LoopTimer);
+    cresce = false;
+    changing = true;
 
-        changeSize()    
-    }
+    changeSize();
+  }
 }
 
+function changeSize() {
+  if (cresce && PorcTime < 100) {
+    circle.style.width = PorcTime + "%";
 
-    function changeSize() {
-        if (cresce && PorcTime < 100) {
-           
-            circle.style.width = PorcTime + '%';
-            
-            
-            PorcTime++;
-             // LOOP
-             if(PorcTime == 100){
-                card.classList.toggle('flip')
-                if(card.classList.contains('flip')){
-                    document.getElementById('button-text').innerHTML = 'Clique e Entre'
-                }else{
-                    document.getElementById('button-text').innerHTML = 'Clique e Cadastre-se'
-                }
-                setTimeout(() => {
-                    PorcTime--
-                }, 10);
-             }
-        
-        }else if(!cresce && PorcTime > 0 && PorcTime !=100){
-                PorcTime--
-                circle.style.width = PorcTime + '%';
-                
-                
-                
-            
-        }
-        else{
-            changing = false;
-            return;
-            
-        }
-        LoopTimer  = setTimeout(changeSize, 0);
-        
-        console.log(PorcTime)
+    PorcTime++;
+    // LOOP
+    if (PorcTime == 100) {
+      card.classList.toggle("flip");
+      if (card.classList.contains("flip")) {
+        document.getElementById("button-text").innerHTML = "Clique e Entre";
+      } else {
+        document.getElementById("button-text").innerHTML =
+          "Clique e Cadastre-se";
+      }
+      setTimeout(() => {
+        PorcTime--;
+      }, 10);
     }
-    
-    
+  } else if (!cresce && PorcTime > 0 && PorcTime != 100) {
+    PorcTime--;
+    circle.style.width = PorcTime + "%";
+  } else {
+    changing = false;
+    return;
+  }
+  LoopTimer = setTimeout(changeSize, 0);
+}
